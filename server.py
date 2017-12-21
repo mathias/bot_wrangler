5d47937a38d2364ba4fe827e9aa86a23c05b94e3
Matt Gauger <matt.gauger@gmail.com>
2017-12-21 16:47:32 -0600

    Extract out dummy Bot class

--- server.py
+++ server.py
@@ -4,21 +4,9 @@ import shlex
 import subprocess
 import time
 import zmq
-import random
 
-class Bot:
-    def __init__(self, name):
-        self.name = name
-        self.first_round = True
-
-    def get_commands(self, input):
-        # If this is the first round, send back name
-        if self.first_round:
-            self.first_round = False
-            return "SimpleBot"
-        else:
-            # For now, always move the first ship randomly
-            return "t 0 1 {}".format(random.randrange(0,359,1))
+# relative imports
+from bot.main import Bot
 
 def parse_game_json(game_output):
     parsed = json.loads(game_output)
