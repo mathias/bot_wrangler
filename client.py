606078a89fde6bc5469c6a3574b5334288f82898
Matt Gauger <mathias@heroku.com>
2017-12-21 13:05:42 -0600

    Allow client to have a port specified

--- client.py
+++ client.py
@@ -1,11 +1,27 @@
 import zmq
 import sys
+import argparse
 
-context = zmq.Context()
 
-sock = context.socket(zmq.REQ)
-sock.connect("tcp://127.0.0.1:5555")
+def _parse_args():
+    parser = argparse.ArgumentParser(description="bot_wrangler client v0")
+    parser.add_argument('-p', '--port', dest='port', help="Port to read/write from for this client", required=True)
 
-for request in range(10):
-    sock.send_string(f"hello from client ##{request}")
-    print(sock.recv())
+    if len(sys.argv) < 2:
+        parser.print_help()
+    return parser.parse_args()
+
+def main():
+    "Run the input/output socket back to the bot_wrangler server"
+    args = _parse_args()
+    port = args.port
+    context = zmq.Context()
+
+    sock = context.socket(zmq.REQ)
+    sock.connect("tcp://127.0.0.1:{}".format(port))
+
+    while True:
+        sock.send_string(f"hello from client")
+        print(sock.recv())
+
+main()
--- server.py
+++ server.py
@@ -13,15 +13,17 @@ socket1.bind("tcp://127.0.0.1:5555")
 
 id = 0
 
-# while True:
-#     print(socket1.recv())
+while True:
+    # command_to_run = """./halite -d "240 160" "echo foo2" "echo foo1" -r -q"""
+    # print(subprocess.check_output(command_to_run, shell=True).decode())
+    print(socket1.recv())
 
-#     id += 1
+    id += 1
 
-#     message = f"Hello #{id}"
-#     socket1.send_string(message)
+    message = f"Hello #{id}"
+    socket1.send_string(message)
 
-for iteration in range(2):
-    command_to_run = """./halite -d "240 160" "echo foo2" "echo foo1" -r -q"""
-    print(subprocess.check_output(command_to_run, shell=True).decode())
+# for iteration in range(2):
+    # command_to_run = """./halite -d "240 160" "echo foo2" "echo foo1" -r -q"""
+    # print(subprocess.check_output(command_to_run, shell=True).decode())
 
