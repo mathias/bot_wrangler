4e9a4cda48682e4adfbf0bd50caa8a3db494beda
Matt Gauger <mathias@heroku.com>
2017-12-21 16:20:20 -0600

    Don't block on ZeroMQ sockets; run until end, parse JSON

--- server.py
+++ server.py
@@ -13,7 +13,8 @@ def parse_game_json(game_output):
         if parsed['stats'][bot_id]['rank'] == 1:
             winner = bot_id
     # TODO mathias: data for computing reward here
-    return { 'winner': winner }
+    #return { 'winner': winner }
+    return parsed
 
 def main():
     # ZeroMQ context
@@ -35,47 +36,51 @@ def main():
 
         proc = subprocess.Popen(shlex.split(command), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 
-        outs = ""
         send_name = True
         while proc.poll() is None:
-            # Bot 1
-            # Read string from halite over socket
-            input = socket1.recv().decode("utf-8") # Map
-            print("Got socket1: {} chars".format(len(input)))
+            try:
+                # Bot 1
+                # Read string from halite over socket
+                input = socket1.recv_string(zmq.NOBLOCK)
+                #print("Got socket1: {} chars".format(len(input)))
 
-            # Form response to halite over socket
-            if send_name:
-                socket1.send_string("SimpleBot1")
-            else:
-                socket1.send_string("") # do nothing, to start
+                # Form response to halite over socket
+                if send_name:
+                    socket1.send_string("SimpleBot1")
+                else:
+                    socket1.send_string("") # do nothing, to start
+            except zmq.ZMQError:
+                continue
 
-            # Bot 2
-            # Read string from halite over socket
-            input = socket2.recv().decode("utf-8") # Map
-            print("Got socket2: {} chars".format(len(input)))
+            try:
+                # Bot 2
+                # Read string from halite over socket
+                input = socket2.recv_string(zmq.NOBLOCK)
+                #print("Got socket2: {} chars".format(len(input)))
 
-            # Form response to halite over socket
-            if send_name:
-                socket2.send_string("SimpleBot2")
-            else:
-                socket2.send_string("") # do nothing, to start
+                # Form response to halite over socket
+                if send_name:
+                    socket2.send_string("SimpleBot2")
+                else:
+                    socket2.send_string("") # do nothing, to start
+            except zmq.ZMQError:
+                continue
 
             send_name = False
-
-            outs += proc.stdout.read()
-            # If we reach endgame, stop doing blocking communication over sockets
-            if len(outs) > 1:
-                break
-
-        while proc.poll() is None:
-            outs += proc.stdout.read()
+        try:
+            outs, errs = proc.communicate(input=None, timeout=10)
+        except TimeoutExpired:
+            proc.kill()
+            outs, errs = proc.communicate(input=None, timeout=10)
+        print("Ended with {}".format(proc.returncode))
 
         # TODO mathias: parse errors
         #if errs:
         #    print(f"Errors: {errs}")
         # else:
         parsed_output = parse_game_json(outs)
-        print("Winner: {}".format(parsed_output['winner']))
+        #print("Winner: {}".format(parsed_output['winner']))
+        print(parsed_output)
 
 if __name__ == '__main__':
     main()
